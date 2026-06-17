6f8ba5bfa44f9462dfa2fd7d51220dc27e3fb3bb
Syahra Zulya Shania Maghfiroh <syahraazly@gmail.com>
2026-06-17 22:26:23 +0700

    feat: add features add plan and component for select category and time

---
+++ TPlanApp/Features/AddPlan/Components/CategoryCard.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCard.swift
+//  TPlanApp
+//
+//  Created by Syahra Zulya Shania Maghfiroh on 17/06/26.
+//
+
+import SwiftUI
+
+struct CategoryCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCard()
+}
---
+++ TPlanApp/Features/AddPlan/Components/TimeRowSelector.swift
@@ -0,0 +1,18 @@
+//
+//  TimeRowSelector.swift
+//  TPlanApp
+//
+//  Created by Syahra Zulya Shania Maghfiroh on 17/06/26.
+//
+
+import SwiftUI
+
+struct TimeRowSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeRowSelector()
+}

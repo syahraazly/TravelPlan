a55db506038cb92a33091a5aaaca5821c4669ed6
Syahra Zulya Shania Maghfiroh <syahraazly@gmail.com>
2026-06-17 13:13:46 +0700

    Initial Commit

---
+++ TPlanApp/TPlanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPlanAppApp.swift
+//  TPlanApp
+//
+//  Created by Syahra Zulya Shania Maghfiroh on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TPlanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
